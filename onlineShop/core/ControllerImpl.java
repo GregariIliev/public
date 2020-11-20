cd5608490e5dbd59ed7a7feab97bbee25cff0ba7
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2020-11-21 01:37:33 +0200

    update ControllerImpl

--- onlineShop/core/ControllerImpl.java
+++ onlineShop/core/ControllerImpl.java
@@ -1,11 +1,13 @@
 package onlineShop.core;
 
 import onlineShop.common.constants.ExceptionMessages;
+import onlineShop.common.constants.OutputMessages;
 import onlineShop.core.interfaces.Controller;
 import onlineShop.models.products.Component;
 import onlineShop.models.products.Computer;
 import onlineShop.models.products.Peripheral;
 import onlineShop.models.products.computers.DesktopComputer;
+import onlineShop.models.products.computers.Laptop;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,20 +19,14 @@ public class ControllerImpl implements Controller {
 
     @Override
     public String addComputer(String computerType, int id, String manufacturer, String model, double price) {
-        String exceptionMessage = "";
-
-        Computer computer = addDesktopComputer(computerType, id, manufacturer, model, price);
-        checkExistComputerInList(computer);
-
-        return exceptionMessage;
-    }
-
-    private Computer addDesktopComputer(String computerType, int id, String manufacturer, String model, double price) {
-        Computer computer = null;
-        if (computerType.equals("DesktopComputer")){
-            computer = new DesktopComputer(id, manufacturer, model, price);
+        Computer computer = createComputer(computerType, id, manufacturer, model, price);
+        for (Computer currentComputer : computerList) {
+            if (computer.getId() == currentComputer.getId()){
+                throw new IllegalArgumentException(ExceptionMessages.EXISTING_COMPUTER_ID);
+            }
         }
-        return computer;
+
+        return String.format(OutputMessages.ADDED_COMPUTER,computer.getId());
     }
 
     @Override
@@ -69,15 +65,17 @@ public class ControllerImpl implements Controller {
         return null;
     }
 
-    private boolean checkExistComputerInList(Computer computer) {
-        boolean exist = false;
 
-        for (Computer computer1 : computerList) {
-            if (computer.getId() == computer1.getId()){
-                exist = true;
-                throw new IllegalArgumentException(ExceptionMessages.EXISTING_COMPUTER_ID);
-            }
+
+    private Computer createComputer(String computerType, int id, String manufacturer, String model, double price) {
+        Computer computer = null;
+        if (computerType.equals("DesktopComputer")){
+            computer = new DesktopComputer(id, manufacturer, model, price);
+        }else if (computerType.equals("Laptop")){
+            computer = new Laptop(id, manufacturer, model, price);
+        }else {
+            throw new IllegalArgumentException(ExceptionMessages.INVALID_COMPUTER_TYPE);
         }
-        return false;
+        return computer;
     }
 }
